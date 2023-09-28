e92222738eada657ae0ca36cb6323b1ffde55bdc
Sven | @greenpixels_ <Sven.Peschau@outlook.com>
2023-09-29 01:29:02 +0200

    feat: add original content warning

---
+++ client/src/components/OriginalContentWarning/OriginalContentWarning.tsx
@@ -0,0 +1,41 @@
+import { Button } from 'antd';
+import Link from 'antd/es/typography/Link';
+
+type OriginalContentWarningProps = {
+	onHide?: () => void;
+};
+
+export default function OriginalContentWarning(
+	props: OriginalContentWarningProps
+) {
+	return (
+		<div>
+			<span>
+				This is one of the original assets of ironsworn. To support the original
+				creators, please visit:
+			</span>
+			<br />
+			<Link
+				href="https://www.ironswornrpg.com/"
+				target="_blank"
+			>
+				https://www.ironswornrpg.com/
+			</Link>
+
+			{props.onHide && (
+				<>
+					<br />
+					<br />
+					<Button
+						ghost
+						type="primary"
+						style={{ border: 'none', padding: '0' }}
+						onClick={() => props.onHide!()}
+					>
+						Hide
+					</Button>
+				</>
+			)}
+		</div>
+	);
+}
---
+++ client/src/types/Metadata.ts
@@ -0,0 +1,7 @@
+export type RessourceMetadata = {
+	username: string;
+	original: boolean;
+	created_on: Date;
+	likes: number;
+	isLiked: boolean;
+};
