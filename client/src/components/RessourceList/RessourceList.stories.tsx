5bf6d9771a6fe576cd8e67e0522292143e3dd827
Sven | @greenpixels_ <Sven.Peschau@outlook.com>
2023-09-29 01:28:18 +0200

    feat: create ressource list to view e.g cards

---
+++ client/src/components/RessourceList/RessourceList.stories.tsx
@@ -0,0 +1,41 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { RessourceList, RessourceType } from './RessourceList';
+import { HawkMockAssetCard } from '../AssetCardViewer/Mocks';
+import { RessourceMetadata } from '../../types/Metadata';
+
+const meta: Meta<typeof RessourceList> = {
+	component: RessourceList,
+};
+
+export default meta;
+type Story = StoryObj<typeof RessourceList>;
+
+const creatorMock: RessourceMetadata = {
+	created_on: new Date(),
+	original: false,
+	username: 'ironswornPlayer154',
+	likes: 100,
+	isLiked: false,
+};
+
+const entriesMock: Array<{
+	ressource: RessourceType;
+	meta: RessourceMetadata;
+}> = [];
+for (let i = 0; i < 100; i++)
+	entriesMock.push({
+		ressource: HawkMockAssetCard,
+		meta: {
+			...creatorMock,
+			likes: Math.round(Math.random() * 100),
+			isLiked: Math.random() > 0.7,
+			original: Math.random() > 0.8,
+		},
+	});
+
+export const Default: Story = {
+	args: {
+		entries: entriesMock,
+		type: 'asset card',
+	},
+};
